879fdd5b5ec303f0077d5f38b1a9ad1b7ea497e7
skeimeier <stefan-keimeier@t-online.de>
2013-01-21 22:05:37 +0100

    dynamisches Nachladen eigefügt

--- source/KathiCurrent.js
+++ source/KathiCurrent.js
@@ -1,15 +1,26 @@
 enyo.kind({
   name: "KathiAktuellesProgramm",
   kind: enyo.Scroller, flex: 1, 
+    published: {
+		urlPage : ""
+    },
+
 	components: [
-			{kind: (window.PalmSystem ? enyo.WebView : enyo.Iframe),
-			name: "currentWebView", flex: 1, 
-			onLoadComplete: "hideWebViewSpinner", 
-			onLoadStarted: "showWebViewSpinner"
+			{kind: "WebView", //(window.PalmSystem ? enyo.WebView : enyo.Iframe),
+			name: "currentWebView", flex: 1 , lazy: false,
+					url : this.urlPage,
+					//onLoadComplete: "hideWebViewSpinner", 
+					//onLoadStarted: "showWebViewSpinner"
 			},
 			{kind: enyo.Spinner, name: "feedWebViewSpinner", align: "center"}
 
 	],
+	create: function() {
+     this.inherited(arguments);
+       enyo.log("Current created:"+this.urlPage);
+		this.$.currentWebView.setUrl(this.urlPage);
+		
+	},
 	rendered: function() {
      this.inherited(arguments);
        enyo.log("Current rendered ");
@@ -21,7 +32,10 @@ enyo.kind({
 	showWebViewSpinner: function() {
 		this.$.feedWebViewSpinner.show();
 	},
-
+	urlPageChanged: function(to_new){
+		this.$.currentWebView.setUrl(to_new);
+	}
+	
 
 		
  
